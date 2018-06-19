e2bc92a34ee8a04b77d6f9655b424a8c40ac79f7
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2018-06-19 14:58:18 +0300

    Добавлен метод hasRole в модель User

--- app/User.php
+++ app/User.php
@@ -1,37 +1,54 @@
 <?php
-
-namespace App;
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password','username',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 	
-	public function groups() {
-		return $this->belongsToMany('App\Group');
-	}
+	namespace App;
 	
-	public function articles() {
-		return $this->hasMany('App\Article');
+	use Illuminate\Notifications\Notifiable;
+	use Illuminate\Foundation\Auth\User as Authenticatable;
+	
+	class User extends Authenticatable {
+		use Notifiable;
+		
+		/**
+		 * The attributes that are mass assignable.
+		 *
+		 * @var array
+		 */
+		protected $fillable = [
+			'name', 'email', 'password', 'username',
+		];
+		
+		/**
+		 * The attributes that should be hidden for arrays.
+		 *
+		 * @var array
+		 */
+		protected $hidden = [
+			'password', 'remember_token',
+		];
+		
+		public function articles() {
+			return $this->hasMany('App\Article');
+		}
+		
+		/**
+		 * Check if User has some role
+		 *
+		 * @param $role - name of group. Field groups.name
+		 *
+		 * @return bool
+		 */
+		public function hasRole($role) {
+			$groups = $this->groups();
+			foreach ($groups as $group) {
+				if ( $group->name == $role ) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public function groups() {
+			return $this->belongsToMany('App\Group');
+		}
 	}
-}
