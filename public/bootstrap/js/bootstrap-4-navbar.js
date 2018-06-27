4142fc14026b09f0db21e7683338976a56c174ee
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2018-06-27 13:40:24 +0300

    Выпадающее меню для мобильных тепрь сразу открыто

--- public/bootstrap/js/bootstrap-4-navbar.js
+++ public/bootstrap/js/bootstrap-4-navbar.js
@@ -6,24 +6,36 @@
 
 
 $(document).ready(function () {
+	var pathPage = location.pathname.slice(1);
+	
+	var parentUl = $('.nav-item a[href*="' + pathPage + '"]').closest('li').addClass('active').parent('ul');
+	var grandParentUl = parentUl.parent('.navbar-nav li').addClass('active').parent('ul');
+	grandParentUl.parent('.navbar-nav li').addClass('active').parent('ul');
+	
 	if (!is_touch_device()) {
 		$('.dropdown-toggle').on('click', function (e) {
 			if ($(this).next('.dropdown-menu').is(':visible')) {
 				window.location = $(this).attr('href');
 			}
 		});
-		
 	}
 	else {
-		$('.dropdown-toggle').on('click', function (e) {
-			if ($(this).next('.dropdown-menu').is(':visible')) {
+		$('.dropdown-menu').css('paddingLeft', '1em');
+		$('.dropdown-toggle')
+			.each(function (o) {
+				var link = $(this).attr('href');
+				if (link && link != '#') {
+					$(this).next(".dropdown-menu").addClass('show');
+				}
+			})
+			.on('click', function (e) {
+				var link = $(this).attr('href');
+				if (link && link != '#') {
 				window.location = $(this).attr('href');
-			}
-			else {
-				$(this).next('.dropdown-menu').css('marginLeft', '1rem');
 			}
 		});
 	}
+	
 	$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
 		var $el = $(this);
 		var $parent = $(this).offsetParent(".dropdown-menu");
