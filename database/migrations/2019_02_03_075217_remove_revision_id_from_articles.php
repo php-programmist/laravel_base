14eaced068777a1254927d0a78333fabd016d196
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-02-03 12:27:01 +0300

    Changed structure of tables: articles, revisions

---
+++ database/migrations/2019_02_03_075217_remove_revision_id_from_articles.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveRevisionIdFromArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign(['revision_id']);
+            $table->dropColumn('revision_id');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('revision_id')->unsigned()->nullable();
+            $table->foreign('revision_id')->references('id')->on('revisions')->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+}
